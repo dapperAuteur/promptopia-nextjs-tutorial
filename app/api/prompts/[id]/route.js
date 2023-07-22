7ec169ca5dc5f21e9d316a8af2b07e051465b368
awe <aweful@awews.com>
2023-07-21 19:02:19 -0700

    return upated object on successful update prompt

--- app/api/prompts/[id]/route.js
+++ app/api/prompts/[id]/route.js
@@ -34,7 +34,7 @@ export const PATCH = async (request, { params }) => {
 
         await existingPrompt.save();
 
-        return new Response("Successfully updated the Prompts", { status: 200 });
+        return new Response(JSON.stringify(existingPrompt), { status: 200 });
     } catch (error) {
         return new Response("Error Updating Prompt", { status: 500 });
     }
